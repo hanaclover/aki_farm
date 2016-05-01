2c243e6f77e8853734b0a9abdb736d159923a8af
meijin <i8858garden@gmail.com>
2016-05-01 18:44:26 +0900

    予約一覧画面
    予約一覧画面についての
    php、js、cssファイルです。
    また、jsフォルダを作成しました

---
+++ bookList.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Owner
+ * Date: 2016/04/28
+ * Time: 18:31
+ */
+?>
+<html>
+<head>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/booklist.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/booklist.css">
+    <title>
+        予約一覧画面
+    </title>
+</head>
+<body>
+<div id="wrapper">
+    <h1>
+        本日の予約リスト
+    </h1>
+    <table border="1px" class="book">
+        <tr>
+            <th>
+                お時間
+            </th>
+            <th>
+                座席番号
+            </th>
+            <th>
+                幹事様お名前
+            </th>
+            <th>
+                電話番号
+            </th>
+        </tr>
+        <tr>
+            <td>
+                19:00
+            </td>
+            <td class="chairNum">
+                <a href="bookList.php">24</a>
+            </td>
+            <td>
+                齊藤様
+            </td>
+            <td>
+                090-1234-5678
+            </td>
+        </tr>
+        <tr>
+            <td>
+                19:00
+            </td>
+            <td class="chairNum">
+                <a href="bookList.php">24</a>
+            </td>
+            <td>
+                齊藤様
+            </td>
+            <td>
+                090-1234-5678
+            </td>
+        </tr>
+        <tr>
+            <td>
+                19:00
+            </td>
+            <td class="chairNum">
+                <a href="bookList.php">24</a>
+            </td>
+            <td>
+                齊藤様
+            </td>
+            <td>
+                090-1234-5678
+            </td>
+        </tr><tr>
+            <td>
+                19:00
+            </td>
+            <td class="chairNum">
+                <a href="bookList.php">24</a>
+            </td>
+            <td>
+                齊藤様
+            </td>
+            <td>
+                090-1234-5678
+            </td>
+        </tr>
+        <tr>
+            <td>
+                19:00
+            </td>
+            <td class="chairNum">
+                <a href="bookList.php">24</a>
+            </td>
+            <td>
+                齊藤様
+            </td>
+            <td>
+                090-1234-5678
+            </td>
+        </tr>
+        <tr>
+            <td>
+                19:00
+            </td>
+            <td class="chairNum">
+                <a href="bookList.php">24</a>
+            </td>
+            <td>
+                齊藤様
+            </td>
+            <td>
+                090-1234-5678
+            </td>
+        </tr>
+        <tr>
+            <td>
+                19:00
+            </td>
+            <td class="chairNum">
+                <a href="bookList.php">24</a>
+            </td>
+            <td>
+                齊藤様
+            </td>
+            <td>
+                090-1234-5678
+            </td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
