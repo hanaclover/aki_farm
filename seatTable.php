adae76b727869cc176316215992e6c2ea6c186f5
JangSooJu <sj_wh0109@naver.com>
2016-05-02 12:06:25 +0900

    Jang
    座席のスタイルを修正しました。(固定値)

--- seatTable.php
+++ seatTable.php
@@ -14,74 +14,67 @@
     <style>
         .test {
             margin: 5px;
-            padding: 50px;
+            text-align: center;
         }
     </style>
     <script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
     <script src="js.js"></script>
-
 </head>
 <body>
-<div style="width: 1300px; height: 123px;">
-    <div style="float: left; margin-right: 30px; width: 600px; height: 120px;">
+<div style="width: 620px; margin: 0 auto">
+    <div style="float: left; margin-right: 10px;">
         <div>
-            <div class="test mouseEvent"
-                 style="float: left; background-color: greenyellow;  width: 90px;"
-                 onclick="location.href='http://localhost/aki_farm/Reserved.php?num=7'">7
-                <?php
-                    $dbdata1 = 0; // StartTime
-                    $dbdata2 = 0; // endTime
-                ?>
-            </div>
-            <div class="test mouseEvent" style="float: left; background-color: greenyellow; width: 90px;"
+            <div class="test mouseEvent" style="float: left; background-color: greenyellow; width: 100px; height: 100px;"
+                 onclick="location.href='http://localhost/aki_farm/Reserved.php?num=7'">7</div>
+            <div class="test mouseEvent" style="float: left; background-color: greenyellow; width: 100px; height: 100px;"
                  onclick="location.href='http://localhost/aki_farm/Reserved.php?num=8'">8</div>
-            <div class="test mouseEvent" style="float: left; background-color: greenyellow; width: 90px;"
+            <div class="test mouseEvent" style="float: left; background-color: greenyellow; width: 100px; height: 100px;"
                  onclick="location.href='http://localhost/aki_farm/Reserved.php?num=9'">9</div>
             <div style="clear: both;"></div>
         </div>
         <div>
-            <div style="float: left; width: 50%;">
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
+            <div style="float: left;">
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 155px; height: 80px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=3'">3</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 155px; height: 80px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=2'">2</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 155px; height: 80px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=1'">1</div>
             </div>
-            <div style="float: left; width: 50%;">
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
+            <div style="float: left;">
+                <div class="test mouseEvent" style="background-color: greenyellow;  width: 155px; height: 80px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=6'">6</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
+                <div class="test mouseEvent" style="background-color: greenyellow;  width: 155px; height: 80px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=5'">5</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
+                <div class="test mouseEvent" style="background-color: greenyellow;  width: 155px; height: 80px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=4'">4</div>
             </div>
             <div style="clear: both"></div>
         </div>
     </div>
-    <div style="float: left; width: 600px; height: 150px;">
+    <div style="float: left; ">
         <div>
-            <div style="float: left; width: 300px;">
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 25%;"
+            <div style="float: left;">
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 130px; height: 86px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=11'">11</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 25%;"
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 130px; height: 86px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=12'">12</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 25%;"
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 130px; height: 86px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=13'">13</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 25%;"
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 130px; height: 86px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=14'">14</div>
             </div>
-            <div style="float: left; width: 300px;">
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
+            <div style="float: left;">
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 130px; height: 62px;"
                      onclick="location.href='http://localhost/aki_farm/Reserved.php?num=21'">21</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=22'">22</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=23'">23</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=24'">24</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=25'">25</div>
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 130px; height: 62px;"
+                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=22'">23</div>
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 130px; height: 62px;"
+                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=23'">25</div>
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 130px; height: 62px;"
+                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=24'">27</div>
+                <div class="test mouseEvent" style="background-color: greenyellow; width: 130px; height: 90px;"
+                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=25'">30</div>
             </div>
             <div style="clear: both"></div>
         </div>
