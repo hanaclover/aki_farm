2e635d396d233a1989322169f98c4bc1aaa6b65d
meijin <i8858garden@gmail.com>
2016-05-06 17:19:33 +0900

    edit basemodel.php

---
+++ class/BaseModel.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * クラスBaseModel
+ * edit　by　YusukeSaitoh　2016/05/06
+ */
+class BaseModel {
+    private $pdo;
+    public function __construct() {
+
+    }
+    public function connect() {
+
+    }
+}
