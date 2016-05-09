a0ea2785c792d1e5949f67135e291f21efd3c621
JangSooJu <sj_wh0109@naver.com>
2016-05-09 16:23:27 +0900

    seatTable.php áóïá

--- class/Reserve.php
+++ class/Reserve.php
@@ -1,6 +1,9 @@
 <?php
 
 class Reserve {
+    private $UID            = "";
+    private $RID            = "";
+    private $SID            = "";
     private $startDay       = "";       //String(Date)  2016-05-06
     private $startTime      = "";       //String(Time)  14:05
     private $reservedTime   = "";       //String(Timestamp)  2016-05-09 14:05:21
@@ -13,35 +16,59 @@ class Reserve {
 		return $this->startDay;
 	}
 	public function setStartDay($startDay) {
-        if(preg_match( '/([0-9]{2})-([0-9]{2})-([0-9]{2})/', $startDay ) === 0 ) {
+
+        $parseDate = explode("-", $startDay);
+
+        if(preg_match( '/([2-9]{1}[0-9]{3})/', $startDay ) === 0 &&
+            checkdate( $parseDate[1], $parseDate[2], $parseDate[0] ) === false ) {
+
             $this->startDay = $startDay;
-        } else {
-            echo "おかしいです";
-        }
+        } else echo "StartDayのタイプが間違いました。";
     }
+
     public function getStartTime() {
         return $this->startTime;
     }
     public function setStartTime($startTime) {
-        $this->startTime = $startTime;
+        if(preg_match( '/([0-9]{2}):([0-9]{2}):([0-9]{2})/', $startTime ) === 0 ) {
+            $this->startTime = $startTime;
+        } else echo "startTimeのタイプが間違いました。";
     }
     public function getReservedTime() {
         return $this->reservedTime;
     }
     public function setReservedTime($reservedTime) {
-        $this->reservedTime = $reservedTime;
+
+        $parseTimeStamp = explode(" ", $reservedTime);      //2016-05-09と14:00:00をわけ
+        $parseDate      = explode("-", $parseTimeStamp[0]); //2016-05-09を-ことにわけ
+
+        if(preg_match( '/([2-9]{1}[0-9]{3})/', $parseDate[0] ) === 0 &&
+            checkdate( $parseDate[1], $parseDate[2], $parseDate[0] ) === false ) {
+
+            echo "ReservedTimeの年度が間違いました。";
+
+            if(preg_match( '/([0-9]{2}):([0-9]{2}):([0-9]{2})/', $parseTimeStamp[1] ) === 0 ) {
+                $this->reservedTime = $reservedTime;
+            }
+
+        } else echo "ReservedTimeが間違いました。";
+
     }
     public function getPeopleNum() {
         return $this->peopleNum;
     }
     public function setPeopleNum($peopleNum) {
-        $this->peopleNum = $peopleNum;
+        if(preg_match( '/([1-9]{1})([0-9]{1})/', $peopleNum ) === 0 ) {
+            $this->peopleNum = $peopleNum;
+        } else echo "peopleNumが間違いました。1~30までです。";
     }
     public function getCourse() {
         return $this->course;
     }
     public function setCourse($course) {
-        $this->course = $course;
+        if($course == 4 || $course == 7 || $course == 10) {
+            $this->course = $course;
+        }
     }
     public function getCourse_flag() {
         return $this->course_flag;
--- seatTable.php
+++ seatTable.php
@@ -14,49 +14,6 @@
     <style>
         .test {
             margin: 5px;
-<<<<<<< HEAD
-            padding: 50px;
-        }
-    </style>
-    <script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
-    <script src="js.js"></script>
-
-</head>
-<body>
-<div style="width: 1300px; height: 123px;">
-    <div style="float: left; margin-right: 30px; width: 600px; height: 120px;">
-        <div>
-            <div class="test mouseEvent"
-                 style="float: left; background-color: greenyellow;  width: 90px;"
-                 onclick="location.href='http://localhost/aki_farm/Reserved.php?num=7'">7
-                <?php
-                    $dbdata1 = 0; // StartTime
-                    $dbdata2 = 0; // endTime
-                ?>
-            </div>
-            <div class="test mouseEvent" style="float: left; background-color: greenyellow; width: 90px;"
-                 onclick="location.href='http://localhost/aki_farm/Reserved.php?num=8'">8</div>
-            <div class="test mouseEvent" style="float: left; background-color: greenyellow; width: 90px;"
-                 onclick="location.href='http://localhost/aki_farm/Reserved.php?num=9'">9</div>
-            <div style="clear: both;"></div>
-        </div>
-        <div>
-            <div style="float: left; width: 50%;">
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=3'">3</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=2'">2</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=1'">1</div>
-            </div>
-            <div style="float: left; width: 50%;">
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=6'">6</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=5'">5</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 50px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=4'">4</div>
-=======
             text-align: center;
         }
     </style>
@@ -91,36 +48,10 @@
                      onclick="location.href='http://localhost/aki_farm/aki_farm/Reserved.php?num=5'">5</div>
                 <div class="test mouseEvent" style="background-color: greenyellow;  width: 185px; height: 105px;"
                      onclick="location.href='http://localhost/aki_farm/aki_farm/Reserved.php?num=4'">4</div>
->>>>>>> refs/remotes/origin/master
             </div>
             <div style="clear: both"></div>
         </div>
     </div>
-<<<<<<< HEAD
-    <div style="float: left; width: 600px; height: 150px;">
-        <div>
-            <div style="float: left; width: 300px;">
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 25%;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=11'">11</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 25%;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=12'">12</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 25%;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=13'">13</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 25%;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=14'">14</div>
-            </div>
-            <div style="float: left; width: 300px;">
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=21'">21</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=22'">22</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=23'">23</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=24'">24</div>
-                <div class="test mouseEvent" style="background-color: greenyellow; height: 10px;"
-                     onclick="location.href='http://localhost/aki_farm/Reserved.php?num=25'">25</div>
-=======
     <div style="float: left; ">
         <div>
             <div style="float: left;">
@@ -144,7 +75,6 @@
                      onclick="location.href='http://localhost/aki_farm/aki_farm/Reserved.php?num=24'">27</div>
                 <div class="test mouseEvent" style="background-color: greenyellow; width: 150px; height: 110px;"
                      onclick="location.href='http://localhost/aki_farm/aki_farm/Reserved.php?num=25'">30</div>
->>>>>>> refs/remotes/origin/master
             </div>
             <div style="clear: both"></div>
         </div>
