7145aae972733e201d41a485c309cb602873ce35
JangSooJu <sj_wh0109@naver.com>
2016-05-09 11:26:28 +0900

    course_flag変数を入れました。

--- class/Reserve.php
+++ class/Reserve.php
@@ -1,11 +1,13 @@
 <?php
+
 class Reserved {
-    private $starDay        = "";   //Date
-    private $startTime      = "";   //Date(Time)
-    private $course         = 0;    //int
-    private $peopleNum      = 0;    //int
-    private $course_4       = "";   //String
-    private $reservedTime   = "";   //Date(Time)
+    private $starDay        = "";       //String(Date)
+    private $startTime      = "";       //String(Time)
+    private $reservedTime   = "";       //String(Time)
+    private $peopleNum      = 0;        //int
+    private $course         = 0;        //int
+    private $course_flag    = false;    //boolean
+    private $course_4       = "";       //String
 
     public function getStarDay() {
 		return $this->starDay;
@@ -19,11 +21,11 @@ class Reserved {
     public function setStartTime($startTime) {
         $this->startTime = $startTime;
     }
-    public function getCourse() {
-        return $this->course;
+    public function getReservedTime() {
+        return $this->reservedTime;
     }
-    public function setCourse($course) {
-        $this->course = $course;
+    public function setReservedTime($reservedTime) {
+        $this->reservedTime = $reservedTime;
     }
     public function getPeopleNum() {
         return $this->peopleNum;
@@ -31,17 +33,24 @@ class Reserved {
     public function setPeopleNum($peopleNum) {
         $this->peopleNum = $peopleNum;
     }
+    public function getCourse() {
+        return $this->course;
+    }
+    public function setCourse($course) {
+        $this->course = $course;
+    }
+    public function getCourse_flag() {
+        return $this->course_flag;
+    }
+    public function setCourse_flag($course_flag) {
+        $this->course_flag = $course_flag;
+    }
     public function getCourse_4() {
         return $this->course_4;
     }
     public function setCourse_4($course_4) {
         $this->course_4 = $course_4;
     }
-    public function getReservedTime() {
-        return $this->reservedTime;
-    }
-    public function setReservedTime($reservedTime) {
-        $this->reservedTime = $reservedTime;
-    }
 }
+
 ?>
