78ac3db21417cff58fd540f1c667491ff90a8d36
JangSooJu <sj_wh0109@naver.com>
2016-05-09 18:08:56 +0900

    processing.php edit + Link edit

--- complete.php
+++ complete.php
@@ -3,18 +3,16 @@
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
-
-
 <body>
 <div id="wrapper">
 <?php include_once('./common/header.html'); ?>
 <?php include_once('./common/nav.html'); ?>
 <h1>
 御予約ありがとうございました。<br>
-ご来店をお待ちしております。</br>
+ご来店をお待ちしております。<br>
 </h1>
 <form action="Reserved.php" method="get">
-    <input type="submit" name="modoru" value="ホームへ" />
+    <input type="submit" name="GoHome" value="ホームへ" />
 </form>
 <?php include_once('./common/footer.html'); ?>
 </div>
