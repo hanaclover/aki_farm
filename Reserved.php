5945a23bd6464e3393065c115f4150e4527c716e
JangSooJu <sj_wh0109@naver.com>
2016-05-10 15:54:57 +0900

    reserved-confirm-processing-complete / processing modifying pls

--- Reserved.php
+++ Reserved.php
@@ -11,8 +11,53 @@
  * 1. ログイン処理
  *
  * */
-
 ?>
+<?php
+/*
+ * 1. 4品の場合 AMPのページに渡す
+ * 2. 4品以外の場合　→　ContentsCheck()
+ * 3. ContentsCheck() RETURN False　→　RESERVEDに戻る
+ *
+ * */
+/*
+include_once("class/Reserve.php");
+
+if( (isset($_POST['course']) == true ? $_POST['course'] : "") == 4 ) {
+    // $_POST['course']が4品の場合、AMPのページに渡す リンク修正!!!!
+    //echo "<script> window.location.href = 'http://localhost/aki_farm/aki_farm/  '; </script>";
+
+} else {
+    //7品、10品の場合
+
+    $reserve = new Reserve();
+    echo "reserve class";
+
+    // $reserve->setUID($_POST['UID']);
+    // $reserve->setRID($_POST['RID']);     일단 없는 상태로 진행
+    // $reserve->setSID($_POST['SID']);     일단 없는 상태로 진행
+
+    $reserve->setPeopleNum($_POST['peopleNum']);
+    $reserve->setReservedTime($_POST['ReservedTime']);
+    $reserve->setStartDay($_POST['Date']);
+
+    //StartTimeをTime型化
+    $startTime = $_POST['hour'].":".$_POST['minute'];
+    $reserve->setStartTime($_POST['startTime']);
+
+    //コース
+    $reserve->setCourse($_POST['course']);
+    echo $_POST['course'];
+    if($reserve->getCourse() == 4) {
+        $reserve->setCourse_flag(true);
+
+        // Array処理が必要です。AMPとの調整が必要
+        $dishName = array($_POST['dishName'][0],$_POST['dishName'][1],$_POST['dishName'][2],$_POST['dishName'][3]);
+        $reserve->setCourse_4($dishName);
+    }
+
+}*/
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -27,92 +72,80 @@
     <?php include_once('./common/header.html'); ?>
     <?php include_once('./common/nav.html'); ?>
     <h2>予約情報を入力してください。</h2>
-    <form action="confirm.php" method="POST">
+    <form action="http://localhost:63342/aki_farm/aki_farm/confirm.php" method="post">
         <table border="1">
             <tr>
                 <td>日にち</td>
                 <td>
-                    <label>
-                        <input type="date" name="Date"  />
-                    </label>
+                    <input type="date" name="Date" />
                 </td>
             </tr>
             <tr>
                 <td>時刻</td>
                 <td>
-                    <label>
-                        <select name="hour">
-                            <option selected value="17">17</option>
-                            <option value="18">18</option>
-                            <option value="19">19</option>
-                            <option value="20">20</option>
-                            <option value="21">21</option>
-                            <option value="22">22</option>
-                            <option value="23">23</option>
-                        </select>時
-                        <select name="minute">
-                            <option selected value="00">00</option>
-                            <option value="10">10</option>
-                            <option value="20">20</option>
-                            <option value="30">30</option>
-                            <option value="40">40</option>
-                            <option value="50">50</option>
-                        </select>分
-                    </label>
+                    <select name="hour">
+                        <option selected value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                    </select>時
+                    <select name="minute">
+                        <option selected value="00">00</option>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="30">30</option>
+                        <option value="40">40</option>
+                        <option value="50">50</option>
+                    </select>分
                 </td>
             </tr>
             <tr>
                 <td>人数</td>
                 <td>
-                    <label>
-                        <input type="number" name="peopleNum" class="unsigned" />
-                    </label>
+                    <input type="number" name="peopleNum" class="unsigned" value="5" placeholder="5" />
                 </td>
             </tr>
             <tr>
                 <td>漢字名前</td>
                 <td>
-                    <input type="text" name="familyName" placeholder="FamilyName" />
-                    <input type="text" name="firstName" placeholder="FirstName" />
+                    <input type="text" name="familyName" placeholder="FamilyName" value="張" />
+                    <input type="text" name="firstName" placeholder="FirstName" value="秀朱" />
                 </td>
             </tr>
             <tr>
                 <td>ふりがな</td>
                 <td>
-                    <input type="text" name="familyName_kana" placeholder="FamilyName" />
-                    <input type="text" name="firstName_kana" placeholder="FirstName" />
+                    <input type="text" name="familyName_kana" placeholder="FamilyName"  value="ジャン" />
+                    <input type="text" name="firstName_kana" placeholder="FirstName"  value="スジュ" />
                 </td>
             </tr>
             <tr>
                 <td>電話番号</td>
                 <td>
-                    <label>
-                        <select name="phoneNum1">
-                            <option selected value="080">080</option>
-                            <option value="090">090</option>
-                            <option value="070">070</option>
-                        </select>-
-                        <input type="number" name="phoneNum2" class="unsigned" />
-                        <input type="number" name="phoneNum3" class="unsigned" />
-                    </label>
+                    <select name="phoneNum1">
+                        <option selected value="080">080</option>
+                        <option value="090">090</option>
+                        <option value="070">070</option>
+                    </select>-
+                    <input type="number" name="phoneNum2" class="unsigned" placeholder="1234"  value="1234" />
+                    <input type="number" name="phoneNum3" class="unsigned" placeholder="5555"  value="5555" />
                 </td>
             </tr>
             <tr>
                 <td>メール</td>
                 <td>
-                    <label>
-                        <input type="text" name="mail" />
-                    </label>
+                    <input type="text" name="mail" placeholder="abc@gmail.com" value="abc@gmail.com"  />
                 </td>
             </tr>
             <tr>
                 <td>コース名</td>
                 <td>
-                    <label>
-                        <input type="radio" name="course" value="4" />4
-                        <input type="radio" name="course" value="7" />7
-                        <input type="radio" name="course" value="10" />10
-                    </label>
+                    <input type="radio" name="course" value="4" />4
+                    <input type="radio" name="course" value="7" checked="checked" />7
+                    <input type="radio" name="course" value="10" />10
                 </td>
             </tr>
         </table>
--- confirm.php
+++ confirm.php
@@ -23,63 +23,70 @@
     <h1>
         以上の内容でよろしいですか？
     </h1>
-<!--    Proccessing.phpで送信および修正の処理を行う-->
-    <form action="Proccessing.php" method="post">
-        <table class="confirm">
-            <tr>
-                <td>
-                    来店日時
-                </td>
-                <td>
-                    <?php echo $_POST["Date"]." ".$_POST["hour"].":".$_POST["minute"]; ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    人数
-                </td>
-                <td>
-                    <?php echo $_POST['peopleNum']; ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    代表者氏名
-                </td>
-                <td>
-                    <?php echo $_POST['familyName']." ".$_POST['firstName']."(".$_POST['familyName_kana']." ".$_POST['firstName_kana'].")"; ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    電話番号
-                </td>
-                <td>
-                    <?php echo $_POST['phoneNum1']."-".$_POST['phoneNum2']."-".$_POST['phoneNum3']; ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    コース名
-                </td>
-                <td>
-                    <?php echo $_POST['course']."品"; ?>
-                </td>
-            </tr>
-            <tr>
-                <td id="no_under_white">
-                    メールアドレス
-                </td>
-                <td>
-                    <?php echo $_POST['mail']; ?>
-                </td>
-            </tr>
-        </table>
-        <div class="btns">
+    <table class="confirm">
+        <tr>
+            <td>
+                来店日時
+            </td>
+            <td>
+                <?php
+                    $StartTime = $_POST["Date"]." ".$_POST["hour"].":".$_POST["minute"];
+                    echo $StartTime;
+                ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                人数
+            </td>
+            <td>
+                <?php echo $_POST["peopleNum"]; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                代表者氏名
+            </td>
+            <td>
+                <?php echo $_POST["familyName"]." ".$_POST["firstName"]."(".$_POST['familyName_kana']." ".$_POST['firstName_kana'].")"; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                電話番号
+            </td>
+            <td>
+                <?php echo $_POST['phoneNum1']."-".$_POST['phoneNum2']."-".$_POST['phoneNum3']; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                コース名
+            </td>
+            <td>
+                <?php echo $_POST['course']."品"; ?>
+            </td>
+        </tr>
+        <tr>
+            <td id="no_under_white">
+                メールアドレス
+            </td>
+            <td>
+                <?php echo $_POST['mail']; ?>
+            </td>
+        </tr>
+    </table>
+    <div class="btns">
+        <form action="Processing.php" method="POST">
+            <input type='hidden' name='Date' value="<?php echo $_POST['Date']; ?>" />
+            <input type='hidden' name='hour' value="<?php echo $_POST['hour']; ?>" />
+            <input type='hidden' name='minute' value="<?php echo $_POST['minute']; ?>" />
+            <input type='hidden' name='peopleNum' value="<?php echo $_POST["peopleNum"]; ?>" />
+            <input type='hidden' name='course' value="<?php echo $_POST['course']; ?>" />
             <input type="hidden" name="reservedTime" value="<?php echo date("Y-m-d H:i:s"); ?>" />
-            <span class="btn"><input type="submit" name="confirm" value="確定" class="sub submit"></span>
-            <span class="btn"><input type="submit" name="confirm" value="修正" class="sub modify"></span>
-        </div>
+        <span class="btn"><input type="submit" name="confirm" value="確定" class="sub submit"></span>
+        <span class="btn"><input type="submit" name="confirm" value="修正" class="sub modify"></span>
+    </div>
     </form>
 <?php include_once('./common/footer.html'); ?>
 </div>
