51ab2e8dda9e2b517d901bef83f8fffc5a4fb4f8
JangSooJu <sj_wh0109@naver.com>
2016-05-10 11:38:38 +0900

    reserve.php modify

--- Proccessing.php
+++ Proccessing.php
@@ -25,11 +25,13 @@ if($_POST['confirm'] == "確定") {
 
     //予約が確定され、SIDとRIDが付与されてるとき
     $reserve = new Reserve();
+
     // <----
     $reserve->setUID($_POST['UID']);
-    $reserve->setRID($_POST['RID']);
-    $reserve->setSID($_POST['SID']);
+    // $reserve->setRID($_POST['RID']);     일단 없는 상태로 진행
+    // $reserve->setSID($_POST['SID']);     일단 없는 상태로 진행
     // ---->
+
     $reserve->setPeopleNum($_POST['peopleNum']);
     $reserve->setReservedTime($_POST['ReservedTime']);
     $reserve->setStartDay($_POST['Date']);
@@ -52,6 +54,8 @@ if($_POST['confirm'] == "確定") {
     // <----------- ModelClassでDataBaseに入れる
 
     // ----------->
+
+
 
     // <----------- メールを送る
         //クライアント
