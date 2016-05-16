c9a2d42e2d983468f299d1271fbf0952a2bf150b
meijin <i8858garden@gmail.com>
2016-05-16 14:37:18 +0900

    美修正

--- dataCheckProcessing.php
+++ dataCheckProcessing.php
@@ -78,28 +78,28 @@ function inputDataCheck($_uid, $_peopleNum, $_startDay, $_startTime, $_phoneNum,
     $inputDate = array();
 
     if(preg_match( '/[0-9]+/', $_uid ));
-    else $inputDate['UID'] = "UIDがおかしいです。";
+    else $inputDate['UID'] = "登録されたユーザーデータがありません";
 
     $parseDate = explode("-", $_startDay);
 
     if(preg_match( '/([2-9]{1}[0-9]{3})/', $parseDate[0] ) &&
         checkdate( $parseDate[1], $parseDate[2], $parseDate[0] )) {
-    } else $inputDate['StartDay'] = "StartDayのタイプが間違いました。";
+    } else $inputDate['StartDay'] = "日付は正しく入力してください";
 
     if(preg_match( '/([0-9]{2}):([0-9]{2}):([0-9]{2})/', $_startTime )) {
-    } else $inputDate['StartTime'] = "startTimeのタイプが間違いました。";
+    } else $inputDate['StartTime'] = "時刻は正しく入力してください";
 
     if( $_peopleNum > 0 && $_peopleNum <= 30 );
-    else $inputDate['peopleNum'] = "1~30人までです。";
+    else $inputDate['peopleNum'] = "お客様の人数は1～30人でお願いします";
 
     if ( preg_match( '/([0-9]{3})-([0-9]{4})-([0-9]{4})/', $_phoneNum ));
     else $inputDate['phoneNum'] = "電話番号は、半角数字で11桁以内で入力してください";
 
     if( $_familyName == '' || $_firstName == '')  {
-        $inputDate['Name'] ="確認お願いいたします。";
+        $inputDate['Name'] ="名前を入力してください";
     }
     if( $_familyName_kana == '' || $_firstName_kana == '')  {
-        $inputDate['Name_kana'] ="確認お願いいたします。";
+        $inputDate['Name_kana'] ="ふりがなを入力してください";
     }
     if ( preg_match('/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+[a-zA-Z0-9\._-]+$/', $_mail ) === 0 )
     {
